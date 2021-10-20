01e2379cc73f5e3abfa4a372f1e02c2aebe9d9ec
User <User@DESKTOP-431TJ68>
2021-10-19 19:36:32 -0700

    Add project files.

---
+++ ERPCoreDemo/Data/ERPContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ERPCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPCoreDemo.Data
+{
+    public class ERPContext:DbContext
+    {
+        public ERPContext(DbContextOptions<ERPContext> options):base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ERPCoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ERPCoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
